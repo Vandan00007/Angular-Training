10abcfe30cdd9859ce4f02c8e0e8da8c64c66cf8
Vandan Patel <vandanpatel@Vandans-MacBook-Pro.local>
2021-11-13 18:39:08 -0500

    directives complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-  
-  
+    BasicHighlightDirective,
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
