c0b41eda2ecd799c4c939e82ef3a115fa0b5f9c5
Vandan Patel <vandanpatel@Vandans-MacBook-Pro.local>
2021-11-14 12:58:53 -0500

    observable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    UserComponent,
+    HomeComponent,
   
   
   ],
